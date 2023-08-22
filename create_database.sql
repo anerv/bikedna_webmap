8862593114b73f4b10c9fc96884f1678691ddd22
Ane Rahbek Vierø <vieroe@hotmail.com>
2023-08-22 15:09:53 +0200

    basic setup

---
+++ create_database.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE bikedna_webmaps ENCODING = 'UTF8';
+
+\c bikedna_webmaps;
+
+CREATE EXTENSION postgis;
+
+CREATE EXTENSION postgis_raster;
+
+CREATE EXTENSION postgis_topology;
+
+\q
